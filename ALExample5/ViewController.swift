a2f525684de771435cc42c981c1038796ea4caf6
byjuanamn <juan.antonio.martin@gmail.com>
2016-06-26 22:51:15 +0200

    Add code to remove
    Añadimos código para borrar de la super view las botones central y dcho.

--- ALExample5/ViewController.swift
+++ ALExample5/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func likeButtonTapped(sender: AnyObject) {
+        sender.removeFromSuperview()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func naButtonTapped(sender: AnyObject) {
+        sender.removeFromSuperview()
     }
-
-
 }
 
